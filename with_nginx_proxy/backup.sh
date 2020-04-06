477c746b1e617b6bdab2821b6e5f4597c242544a
Robert Eckermann <info@roberteckermann.eu>
2020-04-06 12:50:09 +0200

    new backup script

--- with_nginx_proxy/backup.sh
+++ with_nginx_proxy/backup.sh
@@ -11,8 +11,6 @@ exit 2
 fi
 
 
-
-
 STR=${DIRECTORY}
 
 length=${#STR}
@@ -26,7 +24,7 @@ cd ${DIRECTORY}
 
 if [ ! -r "volumes/" ] 
 then 
-echo "Kein Volume Verzeichnis gefunden!"
+echo "Kein Volumes-Ordner in ${DIRECTORY}/ gefunden!"
 exit 2
 fi
 
@@ -54,6 +52,8 @@ echo "     Backup wird komprimiert..."
 echo " "
 zip -r backup_full_${DATESTRING}.zip backup_full_${DATESTRING}/ &> /dev/null
 
+rm -rf backup_full_${DATESTRING}/ &> /dev/null
+
 FILE="backup_full_${DATESTRING}.zip"
 if [ -f "$FILE" ]
 then
@@ -62,29 +62,27 @@ then
     echo "     backup_full_${DATESTRING}.zip"
     echo " "
     echo "     Backup muss mit 'unzip -X -K <filename>' entpackt"
-    echo "     und mit 'rsync -Aaxv --delete <backupname> volumes/' wieder"
+    echo "     und mit 'rsync -Aaxv --delete <backupname>/ volumes/' wieder"
     echo "     eingespielt werden. "
+    echo " "
+    echo "------------------------------------------------------------------------"
+    echo " "
+    echo "     Container werden wieder gestartet..."
+    docker-compose up -d &> /dev/null
+    echo "     Backup wurde erfolgreicht erstellt."
+    echo " "
+    echo "------------------------------------------------------------------------"
     echo " "
 else
     echo "     Backup wurde nicht erstellt!"
     echo " "
     echo "     Es ist ein Fehler aufgetreten."
     echo " "
+    echo "------------------------------------------------------------------------"
+    echo " "
+    echo "     Container werden wieder gestartet..."
+    docker-compose up -d &> /dev/null
+    echo " "
+    echo "------------------------------------------------------------------------"
+    echo " "
 fi
-
-
-
-
-
-
-rm -rf backup_full_${DATESTRING}/
-
-echo "------------------------------------------------------------------------"
-echo " "
-echo "     Container werden wieder gestartet..."
-docker-compose up -d &> /dev/null
-
-echo "     Backup wurde erfolgreicht erstellt."
-echo " "
-echo "------------------------------------------------------------------------"
-echo " "
